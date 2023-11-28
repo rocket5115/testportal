6b6035ee3fa4359f7f2cbdb2962a1d4c7c58068a
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-11-28 19:39:45 +0100

    Update main.php

--- main.php
+++ main.php
@@ -16,4 +16,23 @@
         }
         return true;
     }
+
+    function IsEmailCorrect($email) {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    function CreateUID($pp="XX",$ee="XX",$length=20) {
+        $result = $pp;
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+=';
+        $charLength = strlen($characters);
+        for($i = 0; $i < $length-4; $i++) {
+            $result .= $characters[rand(0,$charLength - 1)];
+        }
+        return $result . $ee;
+    }
+
+    function CreateFreeUID($length=20) {
+        $result = CreateUID(null, null, $length+4);
+        return substr($result, 2, strlen($result)-4);
+    }
 ?>
