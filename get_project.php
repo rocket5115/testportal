58f28f10b348f2cc474605d6ee41a3252d0f937a
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-11-28 19:23:06 +0100

    Update get_project.php
    Minified few lines of code and supressed unimportant info for user

--- get_project.php
+++ get_project.php
@@ -1,12 +1,13 @@
 <?php
-    if(!isset($_COOKIE['username'])or!isset($_COOKIE['password'])or!isset($_COOKIE['project_name'])){
+    require "main.php";
+    if(!IsPostSet(["username","password","project_name"])) {
         die('You must be logged in!');
     };
     $conn = new mysqli("localhost", "root", "", "testportal");
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+        die("Brak rekordów");
     };
-    $result = $conn->query('SELECT projekty.data AS prData FROM projekty INNER JOIN users ON projekty.uid=users.uid WHERE users.username="'.$_COOKIE['username'].'" AND users.password="'.$_COOKIE['password'].'" AND projekty.title="'.$_COOKIE['project_name'].'"');
+    $result = $conn->query('SELECT projekty.data AS prData FROM projekty AS prj JOIN users AS us ON prj.uid=users.uid WHERE us.username="'.$_COOKIE['username'].'" AND us.password="'.$_COOKIE['password'].'" AND prj.title="'.$_COOKIE['project_name'].'"');
     if ($result->num_rows > 0) {
         $row=$result->fetch_assoc();
         echo($row['prData']);
