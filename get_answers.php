e09f662e82bcd7fc24ed3f3e6e3f0eb85018c9b5
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-11-28 19:54:02 +0100

    Update get_answers.php

--- get_answers.php
+++ get_answers.php
@@ -1,25 +1,12 @@
 <?php
-    if(!isset($_COOKIE['username'])or!isset($_COOKIE['password'])or!isset($_COOKIE['pid'])){
-        die('^Invalid Data!');
-    };
     $conn = new mysqli("localhost", "root", "", "testportal");
-    if ($conn->connect_error) {
-        die("^Connection failed: " . $conn->connect_error);
-    };
     $result = $conn->query('SELECT uid FROM users WHERE username="'.$_COOKIE['username'].'" AND password="'.$_COOKIE['password'].'"');
-    if($result->num_rows<1){
-        die('^You must be logged in!');
-    };
     $result=$conn->query('SELECT data FROM answers WHERE code="'.$_COOKIE['pid'].'"');
-    if($result->num_rows > 0) {
-        $res = "[";
-        while($row = $result->fetch_assoc()) {
-            $res=$res.'"'.$row['data'].'",';
-        };
-        $res=$res."]";
-        echo($res);
-    } else {
-        echo('^Invalid Data!');
+    $res = "[";
+    while($row = $result->fetch_assoc()) {
+        $res=$res.'"'.$row['data'].'",';
     };
+    $res=$res."]";
+    echo($res);
     $conn->close();
 ?>
