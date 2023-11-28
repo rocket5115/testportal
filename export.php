706df02899741b4cfb8dadef5a47cd104b23dacf
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-11-28 19:45:19 +0100

    Update export.php
    major minification and use of main.php function

--- export.php
+++ export.php
@@ -1,5 +1,6 @@
 <?php
-if(isset($_COOKIE['username'])and isset($_COOKIE['password'])and isset($_COOKIE['data'])and isset($_COOKIE['answers'])and isset($_COOKIE['pass'])and isset($_COOKIE['title'])){
+require "main.php";
+if(IsPostSet(["username","password","data","answers","pass","title"])) {
     $conn = new mysqli("localhost", "root", "", "testportal");
     if ($conn->connect_error) {
         die('failed Error Connecting to Server');
@@ -11,21 +12,8 @@ if(isset($_COOKIE['username'])and isset($_COOKIE['password'])and isset($_COOKIE[
     } else {
         die('You must be logged in!');
     };
-    function CreateNewPID() {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $randomString = '';
-    
-        for ($i = 0; $i < 6; $i++) {
-            $index = rand(0, strlen($characters) - 1);
-            $randomString .= $characters[$index];
-        }
-        return $randomString;
-    };
-    $password = $_COOKIE['pass'];
-    if($password=='#NOT#SET'){
-        $password="";
-    };
-    $pid = CreateNewPID();
+    $password = $_COOKIE['pass']=="#NOT#SET"?"":$password;
+    $pid = CreateFreeUID(6);
     $conn->query('INSERT INTO exports (title,code,data,answers,uid,pid) VALUES("'.$_COOKIE['title'].'","'.$password.'","'.$_COOKIE['data'].'","'.$_COOKIE['answers'].'","'.$uid.'","'.$pid.'")');
     $conn->close();
     echo('Twoje Nowe ID Testu to: '.$pid);
