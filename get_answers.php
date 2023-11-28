351e2d5482a26189fa53004ea3c562a3692a1b36
rocket5115 <65498427+rocket5115@users.noreply.github.com>
2023-11-28 19:54:28 +0100

    Update get_answers.php

--- get_answers.php
+++ get_answers.php
@@ -1,12 +1,25 @@
 <?php
-    $conn = new mysqli("localhost", "root", "", "testportal");
-    $result = $conn->query('SELECT uid FROM users WHERE username="'.$_COOKIE['username'].'" AND password="'.$_COOKIE['password'].'"');
-    $result=$conn->query('SELECT data FROM answers WHERE code="'.$_COOKIE['pid'].'"');
-    $res = "[";
-    while($row = $result->fetch_assoc()) {
-        $res=$res.'"'.$row['data'].'",';
+    if(!isset($_COOKIE['username'])or!isset($_COOKIE['password'])or!isset($_COOKIE['pid'])){
+        die('^Invalid Data!');
     };
-    $res=$res."]";
-    echo($res);
+    $conn = new mysqli("localhost", "root", "", "testportal");
+    if ($conn->connect_error) {
+        die("^Connection failed: " . $conn->connect_error);
+    };
+    $result = $conn->query('SELECT uid FROM users WHERE username="'.$_COOKIE['username'].'" AND password="'.$_COOKIE['password'].'"');
+    if($result->num_rows<1){
+        die('^You must be logged in!');
+    };
+    $result=$conn->query('SELECT data FROM answers WHERE code="'.$_COOKIE['pid'].'"');
+    if($result->num_rows > 0) {
+        $res = "[";
+        while($row = $result->fetch_assoc()) {
+            $res=$res.'"'.$row['data'].'",';
+        };
+        $res=$res."]";
+        echo($res);
+    } else {
+        echo('^Invalid Data!');
+    };
     $conn->close();
 ?>
